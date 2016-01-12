fc3a477aded38ae47b3ce7952444da52eb8b6ab2
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-01-12 13:49:29 +0900

    Ajaxでツイートを取得する処理を追加

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,5 @@
 $(window).load(function() {
+    /* フォローしてたら青色、してなかったら白色のボタン */
     if ($('#followJudge').val() == "follow") {
         $('.follow').removeClass('btn-default');
         $('.follow').addClass('btn-primary');
@@ -7,6 +8,7 @@ $(window).load(function() {
         $('.follow').addClass('btn-default');
     }
 
+    /* ツイート、フォロー、フォロワーの切り替え */
     if (location.href.match('follow')) {
         $('.userNav li').removeClass('active');
         $('.menu li:nth-child(2)').addClass('active');
@@ -15,3 +17,32 @@ $(window).load(function() {
 //        $('.menu li:nth-child(3)').addClass('active');
     }
 });
+
+
+/* ツイートの取得 */
+setInterval("tweetListAjax()",5000);
+
+function tweetListAjax() {
+    $.ajax({
+        url: 'http://localhost:9000/top',
+        type: 'GET',
+
+    }).success(function(data){
+        var myListLength = $('.tweet-list-area li').length;
+        var ajaxListLength = $(data).find('.tweet-list-area li').length;
+
+        // ちょっと保留（新着のお知らせ的な感じに出るやつ）
+//        if (myListLength < ajaxListLength && $('.new').length < 1) {
+//            var test = $('.tweet-list-area').html();
+//            $('.tweet-list-area').prepend('<li class="list-group-item new">新着ツイートを表示</li>');
+//        }
+//
+//        $('.new').click(function() {
+            var tweetHtml = $(data).find('.tweet-list-area').html();
+            $('.tweet-list-area').html(tweetHtml);
+//        });
+
+    }).error(function(data){
+        alert('error!!!');
+    });
+}
